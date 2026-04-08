1e057ac7f05058e7dee126f8b04f1ebef4186a8e
Benjamincalderonhumareda <benjamincalderonhumareda@gmail.com>
2026-04-07 20:22:01 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,13 @@
-const CACHE_NAME = "aula-mila-v1";
+const CACHE_NAME = "aula-mila-v2";
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
-    })
-  );
+  self.skipWaiting(); // fuerza activación inmediata
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim()); // toma control de la página
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
